898fbff85c5e72aea7deceb3a2ff3447c3a10c4c
Joran <joran@mysocialdatabase.com>
2026-01-17 22:18:33 +0000

    save searches

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Search(models.Model):
+    query = models.TextField()
+    keywords = models.TextField()
+    countries = models.TextField(null=True)
+    query_type = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    size = models.IntegerField(null=True)
 
+    def __str__(self):
+        return self.query
